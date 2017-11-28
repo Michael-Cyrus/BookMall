4b4409b927a4c9ff59fc3003c938581cea4fde8e
“用chenyao <michael_cyrus@163.com>
2017-11-28 15:56:12 +0800

    综合讨论区

--- app/src/main/java/com/michael/bookmall/ui/easyadapter/BookDiscussionAdapter.java
+++ app/src/main/java/com/michael/bookmall/ui/easyadapter/BookDiscussionAdapter.java
@@ -1,10 +1,19 @@
 package com.michael.bookmall.ui.easyadapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.michael.bookmall.R;
+import com.michael.bookmall.base.Constant;
 import com.michael.bookmall.bean.DiscussionList;
+import com.michael.bookmall.manager.SettingManager;
+import com.michael.bookmall.utils.FormatUtils;
+import com.michael.bookmall.utils.LogUtils;
+import com.michael.bookmall.utils.ScreenUtils;
 import com.michael.bookmall.view.recyclerview.BaseViewHolder;
 import com.michael.bookmall.view.recyclerview.RecyclerArrayAdapter;
 
@@ -24,6 +33,48 @@ public class BookDiscussionAdapter extends RecyclerArrayAdapter<DiscussionList.P
         return new BaseViewHolder<DiscussionList.PostsBean>(parent, R.layout.item_community_book_discussion_list) {
             @Override
             public void setData(DiscussionList.PostsBean item) {
+                if (!SettingManager.getInstance().isNoneCover()) {
+                    holder.setCircleImageUrl(R.id.ivBookCover, Constant.IMG_BASE_URL + item.author.avatar,
+                            R.mipmap.avatar_default);
+                } else {
+                    holder.setImageResource(R.id.ivBookCover, R.mipmap.avatar_default);
+                }
+
+                holder.setText(R.id.tvBookTitle, item.author.nickname)
+                        .setText(R.id.tvBookType, String.format(mContext.getString(R.string.book_detail_user_lv), item.author.lv))
+                        .setText(R.id.tvTitle, item.title)
+                        .setText(R.id.tvHelpfulYes, item.commentCount + "")
+                        .setText(R.id.tvLikeCount, item.likeCount + "");
+
+                try {
+                    TextView textView = holder.getView(R.id.tvHelpfulYes);
+                    if (item.type.equals("vote")) {
+                        Drawable drawable = ContextCompat.getDrawable(mContext, R.mipmap.ic_notif_vote);
+                        drawable.setBounds(0, 0, ScreenUtils.dpToPxInt(15), ScreenUtils.dpToPxInt(15));
+                        textView.setCompoundDrawables(drawable, null, null, null);
+                    } else {
+                        Drawable drawable = ContextCompat.getDrawable(mContext, R.mipmap.ic_notif_post);
+                        drawable.setBounds(0, 0, ScreenUtils.dpToPxInt(15), ScreenUtils.dpToPxInt(15));
+                        textView.setCompoundDrawables(drawable, null, null, null);
+                    }
+
+                    if (TextUtils.equals(item.state, "hot")) {
+                        holder.setVisible(R.id.tvHot, true);
+                        holder.setVisible(R.id.tvTime, false);
+                        holder.setVisible(R.id.tvDistillate, false);
+                    } else if (TextUtils.equals(item.state, "distillate")) {
+                        holder.setVisible(R.id.tvDistillate, true);
+                        holder.setVisible(R.id.tvHot, false);
+                        holder.setVisible(R.id.tvTime, false);
+                    } else {
+                        holder.setVisible(R.id.tvTime, true);
+                        holder.setVisible(R.id.tvHot, false);
+                        holder.setVisible(R.id.tvDistillate, false);
+                        holder.setText(R.id.tvTime, FormatUtils.getDescriptionTimeFromDateString(item.created));
+                    }
+                } catch (Exception e) {
+                    LogUtils.e(e.toString());
+                }
             }
         };
     }
